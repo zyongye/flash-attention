9153dc11d5cb779bc06b9809cd7fb4e8b9201604
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2025-06-16 11:20:54 -0700

    api

--- csrc/flash_attn/src/flash.h
+++ csrc/flash_attn/src/flash.h
@@ -140,6 +140,8 @@ struct Flash_fwd_params : public Qkv_params {
 
     bool unpadded_lse;  // For varlen paths: LSE is in [nheads, total_seqlen_q] format instead of [b, nheads, seqlen_q].
     bool seqlenq_ngroups_swapped;  // q has been transposed from (b, 1, (nheads_kv ngroups), d) to (b, ngroups, nheads_kv, d).
+
+    void * __restrict__ s_aux_ptr;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
